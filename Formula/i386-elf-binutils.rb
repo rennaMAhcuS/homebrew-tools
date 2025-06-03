986a85ddeb3550c18ec341a012e42b8d3d815403
Mohana Evuri <148174808+rennaMAhcuS@users.noreply.github.com>
2025-06-03 17:18:21 +0530

    Added the `i386-elf-toolchain` formulae

---
+++ Formula/i386-elf-binutils.rb
@@ -0,0 +1,35 @@
+class I386ElfBinutils < Formula
+  desc "GNU Binutils targetting i386-elf"
+  homepage "https://www.gnu.org/software/binutils"
+  url "https://ftpmirror.gnu.org/gnu/binutils/binutils-2.44.tar.xz"
+  sha256 "ce2017e059d63e67ddb9240e9d4ec49c2893605035cd60e92ad53177f4377237"
+
+  depends_on "gcc"
+
+  on_macos do
+    def install
+      ENV["CC"] = Formula["gcc"].opt_bin/"gcc-#{Formula["gcc"].version.major}"
+      ENV["CXX"] = Formula["gcc"].opt_bin/"g++-#{Formula["gcc"].version.major}"
+
+      mkdir "binutils-build" do
+        system "../configure", "--prefix=#{prefix}",
+                               "--target=i386-elf",
+                               "--with-system-zlib"
+
+        system "make", "-j#{ENV.make_jobs}"
+        system "make", "install"
+      end
+    end
+  end
+
+  test do
+    (testpath/"program.S").write <<~DATA
+    .text
+    example:
+        movl 0x80, %eax
+        movl 0x40, %ebx     
+    DATA
+    system "#{bin}/i386-elf-as", "program.S"
+    assert_match "file format elf32-i386", shell_output("#{bin}/i386-elf-objdump -D a.out")
+  end
+end
---
+++ Formula/i386-elf-gcc.rb
@@ -0,0 +1,49 @@
+class I386ElfGcc < Formula
+  desc "GNU Compiler Collection targetting i386-elf"
+  homepage "https://gcc.gnu.org"
+  url "https://ftpmirror.gnu.org/gnu/gcc/gcc-15.1.0/gcc-15.1.0.tar.xz"
+  sha256 "e2b09ec21660f01fecffb715e0120265216943f038d0e48a9868713e54f06cea"
+
+  depends_on "gmp" => :build
+  depends_on "mpfr" => :build
+  depends_on "libmpc"
+  depends_on "gcc"
+  depends_on "rennamahcus/tools/i386-elf-binutils"
+
+  on_macos do
+    def install
+      ENV["CC"] = Formula["gcc"].opt_bin/"gcc-#{Formula["gcc"].version.major}"
+      ENV["CXX"] = Formula["gcc"].opt_bin/"g++-#{Formula["gcc"].version.major}"
+
+      mkdir "gcc-build" do
+        system "../configure", "--prefix=#{prefix}",
+                              "--target=i386-elf",
+                              "--with-system-zlib",
+                              "--enable-languages=c,c++",
+                              "--with-gmp=#{Formula["gmp"].opt_prefix}",
+                              "--with-mpfr=#{Formula["mpfr"].opt_prefix}",
+                              "--with-mpc=#{Formula["libmpc"].opt_prefix}"
+
+        system "make", "-j#{ENV.make_jobs}", "all-gcc"
+        system "make", "install-gcc"
+        system "make", "-j#{ENV.make_jobs}", "all-target-libgcc"
+        system "make", "install-target-libgcc"
+
+        # Link binutils for GCC to find
+        binutils = Formula["rennamahcus/tools/i386-elf-binutils"].prefix
+        ln_sf "#{binutils}/i386-elf", "#{prefix}/i386-elf"
+      end
+    end
+  end
+
+  test do
+    (testpath/"program.c").write <<~DATA
+    int sum(int a, int b) {
+      return a + b;
+    }
+    DATA
+    system "#{bin}/i386-elf-gcc", "-c", "program.c"
+    binutils = Formula["nativeos/i386-elf-toolchain/i386-elf-binutils"].prefix
+    assert_match "file format elf32-i386", shell_output("#{binutils}/bin/i386-elf-objdump -D program.o")
+  end
+end
