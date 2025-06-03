4373f56e0c07f840cf3af2091d27164569917983
Mohana Evuri <148174808+rennaMAhcuS@users.noreply.github.com>
2025-06-03 19:01:00 +0530

    Updated `dineroiv.rb`

--- Formula/dineroiv.rb
+++ Formula/dineroiv.rb
@@ -4,19 +4,15 @@ class Dineroiv < Formula
   url "https://github.com/atos-tools/dineroIV/archive/refs/heads/master.tar.gz"
   version "4"
   sha256 "6f1b5e2049b061663432aa2d89659258cfa1d857d93f5cd1aa681105ac9110df"
-
-  depends_on "gcc"
-
+  
   def install
-    on_macos do
-      mkdir "dineroIV-build" do
-        system "../configure", "--prefix=#{prefix}"
-        system "make", "-j#{`sysctl -n hw.ncpu`.chomp}"
-        system "make", "install"
+    mkdir "dineroIV-build" do
+      system "../configure", "--prefix=#{prefix}"
+      system "make", "-j#{ENV.make_jobs}"
+      system "make", "install"
 
-        man1.install buildpath/"d4.1" => "dineroIV.1"
-        man3.install buildpath/"d4.3" => "dineroIV.3"
-      end
+      man1.install buildpath/"d4.1" => "dineroIV.1"
+      man3.install buildpath/"d4.3" => "dineroIV.3"
     end
   end
 
@@ -27,11 +23,9 @@ class Dineroiv < Formula
   
     system "curl", "-L", resource_url, "-o", tarball
     system "mkdir", "-p", repo_dir
-    system "tar", "-xzf", tarball, "-C", repo_dir, "--strip-components=1"
+    system "tar", "-xvf", tarball, "-C", repo_dir, "--strip-components=1"
 
     cd repo_dir do
-      ENV["CC"] = Formula["gcc"].opt_bin/"gcc-14"
-      ENV["CXX"] = Formula["gcc"].opt_bin/"g++-14"
       system "./configure"
       cd "testing" do
         system "make"
