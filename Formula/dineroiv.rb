809fd328613e70935ea27d5625a9c459927393b5
Mohana Evuri <148174808+rennaMAhcuS@users.noreply.github.com>
2025-06-22 13:11:42 +0530

    Updated the formulae (#2)
    * Updated the formulae
    
    * Updated the formulae
    
    * Updated dineroiv

--- Formula/dineroiv.rb
+++ Formula/dineroiv.rb
@@ -4,34 +4,29 @@ class Dineroiv < Formula
   url "https://github.com/atos-tools/dineroIV/archive/refs/heads/master.tar.gz"
   version "4"
   sha256 "6f1b5e2049b061663432aa2d89659258cfa1d857d93f5cd1aa681105ac9110df"
+  # license "LicenseRef-DineroIV"
 
   def install
-    mkdir "dineroIV-build" do
-      system "../configure", "--prefix=#{prefix}"
-      system "make", "-j#{ENV.make_jobs}"
-      system "make", "install"
+    system "./configure", *std_configure_args
+    system "make", "-j#{ENV.make_jobs}"
 
-      man1.install buildpath/"d4.1" => "dineroIV.1"
-      man3.install buildpath/"d4.3" => "dineroIV.3"
+    cd "testing" do
+      system "make"
+      with_env(PATH: "#{ENV["PATH"]}:..") do
+        system "./testscript"
+        with_env(D4_SRC: "..") do
+          system "./testscript", "-c"
+        end
+      end
     end
+
+    system "make", "install"
+
+    man1.install "./d4.1" => "dineroIV.1"
+    man3.install "./d4.3" => "dineroIV.3"
   end
 
   test do
-    resource_url = "https://github.com/atos-tools/dineroIV/archive/refs/heads/master.tar.gz"
-    tarball = "dineroIV.tar.gz"
-    repo_dir = testpath/"dineroIV"
-
-    system "curl", "-L", resource_url, "-o", tarball
-    mkdir_p repo_dir
-    system "tar", "-xvf", tarball, "-C", repo_dir, "--strip-components=1"
-
-    cd repo_dir do
-      system "./configure"
-      cd "testing" do
-        system "make"
-        system "./testscript"
-        system "./testscript", "-c"
-      end
-    end
+    system "#{bin}/dineroIV", "-help"
   end
 end
