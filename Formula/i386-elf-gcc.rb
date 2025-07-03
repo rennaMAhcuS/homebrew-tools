c20492e3cd9b7948eb51f9998daa901534266fd4
Mohana Evuri <148174808+rennaMAhcuS@users.noreply.github.com>
2025-07-03 16:49:20 +0530

    Minor changes

--- Formula/i386-elf-gcc.rb
+++ Formula/i386-elf-gcc.rb
@@ -2,6 +2,7 @@ class I386ElfGcc < Formula
   desc "GNU Compiler Collection targetting i386-elf"
   homepage "https://gcc.gnu.org"
   url "https://ftp.gnu.org/gnu/gcc/gcc-15.1.0/gcc-15.1.0.tar.xz"
+  url "https://ftpmirror.gnu.org/gnu/gcc/gcc-15.1.0/gcc-15.1.0.tar.xz"
   mirror "https://ftpmirror.gnu.org/gnu/gcc/gcc-15.1.0/gcc-15.1.0.tar.xz"
   sha256 "e2b09ec21660f01fecffb715e0120265216943f038d0e48a9868713e54f06cea"
 
